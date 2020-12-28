669e6345727329edad33da0f1d41f0b39aa15d62
niekas <albertas.gimbutas@mii.vu.lt>
2020-12-28 14:44:19 +0000

    Add drf-yasg to installed apps

--- reservations/settings/base.py
+++ reservations/settings/base.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservations',
+
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
+
+    'reservations',
 ]
 
 MIDDLEWARE = [

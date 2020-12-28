bac8755091eaaa8694a1cb5f2ed2d2302f0e8f72
niekas <albertas.gimbutas@mii.vu.lt>
2020-12-28 18:23:36 +0000

    Add MeetingRoom and Reservation models

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,27 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class Employee(AbstractUser):
     def __str__(self):
         return self.username
+
+
+class MeetingRoom(models.Model):
+    number = models.CharField(max_length=32)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    capacity = models.IntegerField(null=True, blank=True)
+    reservations = models.ManyToManyField(Employee, through='Reservation')
+
+
+class Reservation(models.Model):
+    room = models.ForeignKey(MeetingRoom, on_delete=models.CASCADE)
+    booked_by = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        related_name='reservations'
+    )
+    title = models.CharField(max_length=255, null=True, blank=True)
+    booked_from = models.DateTimeField()
+    booked_till = models.DateTimeField()
+    attendees = models.ManyToManyField(Employee, related_name='invited_to')

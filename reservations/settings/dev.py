a3a4cd3a3b5d211dbebdf1e42906451535f67d29
niekas <albertas.gimbutas@mii.vu.lt>
2020-12-29 13:39:22 +0000

    Add logging for all API endpoints

--- reservations/settings/dev.py
+++ reservations/settings/dev.py
@@ -16,3 +16,35 @@ DATABASES = {
         "PORT": 5432,
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "verbose"},
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": str(BASE_DIR / ".." / "info.log"),  # noqa: F405
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "django": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth import get_user_model
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
@@ -10,6 +12,8 @@ from reservations.serializers import (EmployeeSerializer,
                                       MeetingRoomSerializer,
                                       ReservationSerializer)
 
+logger = logging.getLogger(__name__)
+
 Employee = get_user_model()
 
 
@@ -17,10 +21,70 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+    def list(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET employee-list with args {dict(request.query_params)}"
+        )
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} POST employee-list with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET employee {pk} with args {dict(request.query_params)}"
+        )
+        return super().retrieve(request, pk, *args, **kwargs)
+
+    def partial_update(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} PATCH employee {pk} with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().partial_update(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        logger.info(f"User {request.user} DELETE employee {pk}")
+        return super().destroy(request, pk, *args, **kwargs)
+
 
 class MeetingRoomViewSet(viewsets.ModelViewSet):
     queryset = MeetingRoom.objects.all()
     serializer_class = MeetingRoomSerializer
+
+    def list(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET meeting-room-list with args {dict(request.query_params)}"
+        )
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} POST meeting-room-list with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET meeting-room {pk} with args {dict(request.query_params)}"
+        )
+        return super().retrieve(request, pk, *args, **kwargs)
+
+    def partial_update(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} PATCH meeting-room {pk} with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().partial_update(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        logger.info(f"User {request.user} DELETE meeting-room {pk}")
+        return super().destroy(request, pk, *args, **kwargs)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
@@ -28,6 +92,36 @@ class ReservationViewSet(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
     filter_backends = [filters.DjangoFilterBackend]
     filterset_class = ReservationFilter
+
+    def list(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET reservation-list with args {dict(request.query_params)}"
+        )
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.info(
+            f"User {request.user} POST reservation-list with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} GET reservation {pk} with args {dict(request.query_params)}"
+        )
+        return super().retrieve(request, pk, *args, **kwargs)
+
+    def partial_update(self, request, pk, *args, **kwargs):
+        logger.info(
+            f"User {request.user} PATCH reservation {pk} with args "
+            f"{dict(request.query_params)} and data {request.data}"
+        )
+        return super().partial_update(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        logger.info(f"User {request.user} DELETE reservation {pk}")
+        return super().destroy(request, pk, *args, **kwargs)
 
 
 def index(request):
